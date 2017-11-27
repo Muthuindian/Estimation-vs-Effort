efaba938249f684fbd2d634d3c5bd5bb5c67eee1
Marimuthu P <muthu.itian@gmail.com>
2017-11-27 13:48:59 +0530

    Task and Assignment Completed

--- src/js/pages/assignments.js
+++ src/js/pages/assignments.js
@@ -1,43 +1,247 @@
 $(document).ready(function() {
 
-
-$(document).on('click' , '#assignments' , function(e) {
-
-	e.stopImmediatePropagation();
-  $('#assignment').addClass('active');
-  $('#task').removeClass('active');
-  $('#project').removeClass('active');
-  $('#time').removeClass('active');
-	list();
-	
-
-});
+    var base_url = "http://localhost:8888/api/timeSheet/task_assignment";
+    var base_project_url = "http://localhost:8888/api/timeSheet/project";
+    var base_employee_url = "http://localhost:8888/api/employee";
+    var base_task_url = "http://localhost:8888/api/timeSheet/task";
+    var assignments;
+    var assignment_id;
+    var task_id;
+    var project_id;
+    var employee_id;
 
 
-$(document).on('click', '#create_assignment_btn', function(e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    $('section').html('');
-    $('section').load('./pages/addAssignments.html', function() {
-      $("#project_name").focus();
+    async function render(calback = function() {}) {
+        await $.ajax({
+            url: base_employee_url,
+            method: "GET",
+            dataType: "json"
+        }).done(function(response) {
+            let data = response;
+            for (var i in data) {
+                var $opt = $('<option id=' + data[i].employee_id + '>');
+                $opt.val(data[i].name).text();
+                $opt.appendTo('#list_employees');
+            }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        });
+        await $.ajax({
+            url: base_project_url,
+            method: "GET",
+            dataType: "json"
+        }).done(function(response) {
+            let data = response;
+            for (var i in data) {
+                var $opt = $('<option id=' + data[i].project_id + '>');
+                $opt.val(data[i].name).text();
+                $opt.appendTo('#list_projects');
+            }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        });
+        await $.ajax({
+            url: base_task_url,
+            method: "GET",
+            dataType: "json"
+        }).done(function(response) {
+            let data = response;
+            for (var i in data) {
+                var $opt = $('<option id=' + data[i].task_id + '>');
+                $opt.val(data[i].task).text();
+                $opt.appendTo('#list_tasks');
+            }
+            calback();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        });
+    }
+
+
+    $(document).on('click', '#assignments', function(e) {
+
+        e.stopImmediatePropagation();
+        $('#assignment').addClass('active');
+        $('#task').removeClass('active');
+        $('#project').removeClass('active');
+        $('#time').removeClass('active');
+        list();
     });
-  });
 
 
-$(document).on('submit', '#assignment', function(e) {
+    $(document).on('click', '#create_assignment_btn', function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        $('section').html('');
+        $('section').load('./pages/addAssignments.html', function() {
+            $("#employee").focus();
+            render();
+        });
+    });
 
-	e.preventDefault();
-    e.stopImmediatePropagation();
-    list();
-    
-  });
+
+    $(document).on('submit', '#assignment', function(e) {
+
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        if ($.cookie("isEdit") == "true")
+            put();
+        else
+            post();
+    });
 
 
-function list() {
+    $(document).on('click', '.btn_deleteassignment', function(e) {
 
-	$('section').html('');
-    $('section').load('./pages/listAssignments.html');
-}
+        console.log("Delete");
+        assignment_id = $(this).closest('tr').attr('id');
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        deleteAssignment(assignment_id);
+    });
 
+    $(document).on('click', '.btn_editassignment', function(e) {
+
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        console.log("Edit");
+        $.cookie("isEdit", true);
+        assignment_id = $(this).closest('tr').attr('id');
+        console.log("id", assignment_id);
+
+        $.ajax({
+            type: "GET",
+            url: base_url + "/" + assignment_id,
+            dataType: "json",
+            success: function(response, status) {
+                $('section').html('');
+                $('section').load('./pages/addAssignments.html', function() {
+                    let data = response;
+                    let callback = function() {
+                        $("#employee").val(data.employee.name).focus();
+                        $("#project").val(data.project.name).focus();
+                        $("#task").val(data.task.task).focus();
+                    };
+                    $("label").addClass('active');
+                    render(callback);
+                });
+            }
+        });
+    });
+
+
+    function deleteAssignment(assignment_id) {
+        console.log("Delete");
+
+        $.ajax({
+            method: "DELETE",
+            url: base_url + "/" + assignment_id,
+            contentType: "application/json",
+            dataType: "json",
+        }).done(function(data, status) {
+            list();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            $('#devision #error_message').html('');
+            $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+        });
+    };
+
+
+    function post() {
+
+        employee_id = ($("#list_employees option[value='" + $('#employee').val() + "']").attr('id'));
+        project_id = ($("#list_projects option[value='" + $('#project').val() + "']").attr('id'));
+        task_id = ($("#list_tasks option[value='" + $('#task').val() + "']").attr('id'));
+        console.log("emp", employee_id);
+        console.log("proj", project_id);
+        console.log("task", task_id);
+        var data = {
+            employee_id: employee_id,
+            project_id: project_id,
+            task_id: task_id
+        };
+
+        $.ajax({
+            method: "POST",
+            url: base_url,
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(data),
+        }).done(function(data, status) {
+            list();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            $('#devision #error_message').html('');
+            $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+        });
+    };
+
+
+    function put() {
+
+        employee_id = ($("#list_employees option[value='" + $('#employee').val() + "']").attr('id'));
+        project_id = ($("#list_projects option[value='" + $('#project').val() + "']").attr('id'));
+        task_id = ($("#list_tasks option[value='" + $('#task').val() + "']").attr('id'));
+        var data = {
+
+            employee_id: employee_id,
+            project_id: project_id,
+            task_id: task_id
+        };
+
+        $.ajax({
+            type: "PUT",
+            url: base_url + "/" + assignment_id,
+            dataType: "json",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function(data, status) {
+                list();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $('#devision #error_message').html('');
+                $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+            }
+        });
+    };
+
+
+
+
+
+
+
+
+    function list() {
+
+        $.cookie("isEdit", false);
+        $('section').html('');
+        $('section').load('./pages/listAssignments.html', function() {
+
+            $.ajax({
+                type: "GET",
+                url: base_url,
+                dataType: "json",
+                success: function(response, status) {
+                    let data = response;
+                    assignments = data;
+                    if (data.length > 0) {
+                        $("#tblassignList tbody").html("");
+                        for (var i in data) {
+                            var no = parseInt(i) + 1;
+                            $("#tblassignList tbody").append("<tr id=" + data[i].task_assignment_id + ">" +
+                                "<td>" + no + " </td><td style='text-align:center;'> " + data[i].employee.name + "</td>" +
+                                "<td style='text-align:center;'> " + data[i].project.name + "</td>" +
+                                "<td style='text-align:center;'> " + data[i].task.task + "</td>" +
+                                "<td  name='" + data[i].employee.name + "' id='" + data[i].task_assignment_id + "' style='text-align:left;' class=''><i  class='material-icons btn_editassignment' style='padding-right:.5rem;'>edit</i>&nbsp;<i href='#modal1' class='material-icons btn_deleteassignment' style='padding-right:1rem;'>delete</i></td>" +
+                                "</tr>");
+                        }
+                    } else {
+                        $("#search_devision_list empty").html('')
+                        $("#search_devision_list empty").append('<div style="padding:4rem;">Please <a id="create_devision_btn">Click Here </a> to create a new Divisions.</div>');
+                    }
+                }
+            });
+        });
+    }
 
 });
--- src/js/pages/tasks.js
+++ src/js/pages/tasks.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
+  var base_url = "http://localhost:8888/api/timeSheet/task";
+  var base_project_url = "http://localhost:8888/api/timeSheet/project" ;
+  var tasks;
+  var task_id;
+  var project_id;
+
+
+
+  function project() {
+      $.ajax({
+        url: base_project_url,
+        method: "GET",
+        dataType: "json"
+      }).done(function(response) {
+        let data = response;
+        for (var i in data) {
+            var $opt = $('<option id=' + data[i].project_id + '>');
+            $opt.val(data[i].name).text();
+            $opt.appendTo('#list_projects');
+        }
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log(errorThrown);
+      });
+    };
+
 
 $(document).on('click' , '#tasks' , function(e) {
 
@@ -20,6 +45,7 @@ $(document).on('click', '#create_task_btn', function(e) {
     $('section').html('');
     $('section').load('./pages/addTasks.html', function() {
       $("#project_name").focus();
+      project();
     });
   });
 
@@ -27,16 +53,154 @@ $(document).on('click', '#create_task_btn', function(e) {
 $(document).on('submit', '#task', function(e) {
 
 	e.preventDefault();
-    e.stopImmediatePropagation();
-    list();
-    
+  e.stopImmediatePropagation();
+  if($.cookie("isEdit") == "true")
+    put();
+  else
+    post();
   });
+
+$(document).on('click', '.btn_deletetask', function(e) {
+
+console.log("Delete");
+  task_id = $(this).closest('tr').attr('id');
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  deleteTask(task_id);    
+  });
+
+$(document).on('click', '.btn_edittask', function(e) {
+
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  console.log("Edit");
+  $.cookie("isEdit" , true);
+  task_id = $(this).closest('tr').attr('id');
+
+  $.ajax({
+        type: "GET",
+        url: base_url + "/" + task_id,
+        dataType: "json",
+        success: function(response, status) {
+          $('section').html('');
+          $('section').load('./pages/addTasks.html', function() {
+            let data = response;
+            $("#project_name").val(data.project.name).focus();
+            $("#task_name").val(data.task).focus();
+            $("#start_date").val(data.start_date).focus();
+            $("#end_date").val(data.end_date).focus();
+            $("#effort").val(data.plan_effort).focus();
+            $("label").addClass('active');
+            project();
+          });
+        }
+      });
+  });
+
+
+function deleteTask(task_id) {
+  console.log("Delete");
+  
+    $.ajax({
+      method: "DELETE",
+      url: base_url + "/" + task_id,
+      contentType: "application/json",
+      dataType: "json",
+    }).done(function(data, status) {
+      list();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('#devision #error_message').html('');
+      $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+    });
+  };
+
+
+function post() {
+
+  project_id = ($("#list_projects option[value='" + $('#project_name').val() + "']").attr('id'));
+    var data = {
+      project_id: project_id,
+      task: $("#task_name").val().trim(),
+      start_date: $("#start_date").val().trim(),
+      end_date: $("#end_date").val().trim(),
+      plan_effort: $("#effort").val().trim()
+    };
+    $.ajax({
+      method: "POST",
+      url: base_url,
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify(data),
+    }).done(function(data, status) {
+      list();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('#devision #error_message').html('');
+      $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+    });
+  };
+
+
+  function put() {
+   
+    project_id = ($("#list_projects option[value='" + $('#project_name').val() + "']").attr('id'));
+    var data = {
+      project_id: project_id,
+      task: $("#task_name").val().trim(),
+      start_date: $("#start_date").val().trim(),
+      end_date: $("#end_date").val().trim(),
+      plan_effort: $("#effort").val().trim()
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: base_url + "/" + task_id,
+      dataType: "json",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      success: function(data, status) {
+        list();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $('#devision #error_message').html('');
+        $('#devision #error_message').append('<span>' + jqXHR.responseJSON.message + '</span>');
+      }
+    });
+  };
+
+
+
 
 
 function list() {
 
-	$('section').html('');
-    $('section').load('./pages/listTasks.html');
+  $.cookie("isEdit", false);
+  $('section').html('');
+    $('section').load('./pages/listTasks.html' , function(){
+
+      $.ajax({
+        type: "GET",
+        url: base_url,
+        dataType: "json",
+        success: function(response, status) {
+          let data = response;
+          tasks = data;
+          if (data.length > 0) {
+            $("#tbltasksList tbody").html("");
+            for (var i in data) {
+              var no = parseInt(i) + 1;
+              $("#tbltasksList tbody").append("<tr id=" + data[i].task_id + ">" +
+                "<td>" + no + " </td><td style='text-align:center;'> " + data[i].project.name + "</td>" +
+                "<td style='text-align:center;'> " + data[i].task + "</td>" +
+                "<td  name='" + data[i].task + "' id='" + data[i].task_id + "' style='text-align:left;' class=''><i  class='material-icons btn_edittask' style='padding-right:.5rem;'>edit</i>&nbsp;<i href='#modal1' class='material-icons btn_deletetask' style='padding-right:1rem;'>delete</i></td>" +
+                "</tr>");
+            }
+          } else {
+            $("#search_devision_list empty").html('')
+            $("#search_devision_list empty").append('<div style="padding:4rem;">Please <a id="create_devision_btn">Click Here </a> to create a new Divisions.</div>');
+          }
+        }
+      });
+    });
 }
 
 
